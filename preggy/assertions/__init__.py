8f67ba46b9456b0e8d43343eb837ed8c30ea7adc
Zearin <zearin@gonk.net>
2013-05-04 13:40:33 -0400

    Moved all assertions under `preggy.assertions`
    When refactoring PyVows to use preggy, I discovered there was no easy way to say,  “import all assertion functions from preggy”.
    
    Instead, it would have required something like this:
    
        from preggy import to_be_empty, to_be_null, to_be_like, to_length ...
        ...
    
    Which isn’t fun.
    
    Instead, this should allow code to just say:
    
        from preggy.assertions import *

---
+++ preggy/assertions/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import 
+
+from preggy.assertions.emptiness import *
+from preggy.assertions.equality import *
+from preggy.assertions.inclusion import *
+from preggy.assertions.length import *
+from preggy.assertions.like import *
+
+from preggy.assertions.types import *
