617640f21156c65b6e7b1c242a2aba01c6e5684d
Bernardo Heynemann <heynemann@gmail.com>
2013-05-16 18:58:36 -0300

    Release 0.5.6

--- preggy/__init__.py
+++ preggy/__init__.py
@@ -11,7 +11,7 @@
 
 from __future__ import absolute_import
 
-__version__ = "0.5.5"
+__version__ = "0.5.6"
 
 from preggy.core import assertion, create_assertions
 from preggy.core import Expect as expect
