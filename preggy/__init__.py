3b68662d3ba35d39e6794df2e0ae4d20bcfbec25
Zearin <zearin@gonk.net>
2013-06-05 14:41:11 -0400

    fix: use: Minor bugfix release (`to_be_like()`)

--- preggy/__init__.py
+++ preggy/__init__.py
@@ -11,7 +11,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '0.5.11'
+__version__ = '0.5.12'
 
 from preggy.core import assertion, create_assertions
 from preggy.core import Expect as expect
