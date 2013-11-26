7bad99a139593d24640e97038fa1847627ae944c
Bernardo Heynemann <heynemann@gmail.com>
2013-11-26 17:49:06 -0200

    release 0.8.0 - comparing uuids

--- preggy/__init__.py
+++ preggy/__init__.py
@@ -11,7 +11,7 @@
 
 from __future__ import absolute_import
 
-__version__ = '0.7.0'
+__version__ = '0.8.0'
 
 from preggy.core import (assertion, create_assertions, fix_string, Expect as expect)
 from preggy.assertions import *
