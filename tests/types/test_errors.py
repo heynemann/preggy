7b78efb4a75c09491d9712440c043597ffd45309
Zearin <zearin@gonk.net>
2014-01-08 13:34:35 -0500

    tests/types/test_errors: Expanded tests

--- tests/types/test_errors.py
+++ tests/types/test_errors.py
@@ -11,16 +11,60 @@ from preggy import expect
 
 #-----------------------------------------------------------------------------
 
+
+
 def test_is_error():
-    expect(RuntimeError()).to_be_an_error()
-    expect(RuntimeError('Something Wrong')).to_be_an_error_like(RuntimeError)
-    expect(RuntimeError('Something Wrong')).to_have_an_error_message_of('Something Wrong')
+    topic = RuntimeError('Something Wrong') 
+    expect(topic).to_be_an_error()
+    expect(topic).to_be_an_error_like(RuntimeError)
+    expect(topic).to_have_an_error_message_of('Something Wrong')
+    
+    topic = ValueError('some bogus error')
+    expect(topic).to_be_an_error()
+    expect(topic).to_be_an_error_like(ValueError)
+    expect(topic).to_have_an_error_message_of('some bogus error')
 
 
 def test_not_to_be_an_error():
-    expect('b123a').Not.to_be_an_error()
-    expect('b123a').not_to_be_an_error()
+    NON_ERRORS = frozenset([
+        0,
+        2,
+        tuple(),
+        object(),
+        'b123a',
+        r'b123a',
+        u'b123a',
+        b'b123a'
+    ])
+    
+    for item in NON_ERRORS:
+        expect(item).Not.to_be_an_error()
+        expect(item).not_to_be_an_error()
+        
+        # try:
+        #     expect(item).to_be_an_error()
+        # except AssertionError as err:
+        #     expect(err).to_have_an_error_message_of("Expected topic({0}) to be an error".format(item))
+            
 
+
+def test_error_messages():
+    topic = Exception('1 does not equal 2')
+    expect(topic).to_have_an_error_message_of('1 does not equal 2')
+    
+    try:
+        expect(topic).to_have_an_error_message_of('some bogus')
+    except AssertionError as err:
+        e_format = "Expected topic({0!r}) to be an error with message {1!r}"
+        e_values = str(topic), 'some bogus'
+        e_message = e_format.format(*e_values)
+        expect(err).to_have_an_error_message_of(e_message)
+    
+    try:
+        expect(2).to_be_an_error()
+    except AssertionError as err:
+        expect(err).to_have_an_error_message_of('Expected topic(2) to be an error')
+    
 
 def test_not_to_be_an_error_like():
     try:
