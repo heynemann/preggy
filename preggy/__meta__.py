2dac8f76dc549b3c9db3a6f5ea0aa2e86103d9bc
Bernardo Heynemann <heynemann@gmail.com>
2014-10-02 11:55:01 -0300

    Release 1.1.3

--- preggy/__meta__.py
+++ preggy/__meta__.py
@@ -18,8 +18,8 @@ from datetime import date
 #   GENERAL
 #--------------------------------------------------------------------------------
 __name__        = 'preggy'  # normally preggy.__meta__
-__version__     = '1.1.2'
-__date__        = date(2014, 8, 29)  # TODO: auto-populate each release using a git hook
+__version__     = '1.1.3'
+__date__        = date(2014, 10, 2)  # TODO: auto-populate each release using a git hook
 __keywords__    = 'test testing assert assertion library development'
 __status__      = 'Production'
 
