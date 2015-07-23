bc244d2566796fa53dc4c921fe3a4dce77022a43
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2015-07-23 19:11:05 -0300

    refact(tests_classes): support python3 making that class hashable
    Also, adhere to preggy's syntax style (or kind of)

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,1 +1,31 @@
 # -*- coding: utf-8 -*-
+
+
+class Comparable(object):
+
+    def __init__(self, foo='bar'):
+        self.foo = foo
+
+    def __hash__(self):
+        return hash(self.foo)
+
+    def __lt__(self, other):
+        return self.foo < other.foo
+
+    def __le__(self, other):
+        return self.foo <= other.foo
+
+    def __eq__(self, other):
+        return self.foo == other.foo
+
+    def __ne__(self, other):
+        return self.foo != other.foo
+
+    def __gt__(self, other):
+        return self.foo > other.foo
+
+    def __ge__(self, other):
+        return self.foo >= other.foo
+
+    def __cmp__(self, other):
+        return cmp(self.foo, other.foo)
